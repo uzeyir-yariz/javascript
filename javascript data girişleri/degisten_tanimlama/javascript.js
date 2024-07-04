68383893a42e55f5b3f75dd128887672ab79750d
uzeyir-yariz <uzeyiryariz13@gmail.com>
2024-07-04 08:21:58 +0300

    değişken tanımlama, string ifadeler eklendi

---
+++ javascript data girişleri/degisten_tanimlama/javascript.js
@@ -0,0 +1,12 @@
+let demo = 10;
+console.log("değer = " + demo);
+
+demo = 15
+console.log(demo + " tekrar değiştirme gerçekleştirildi");
+
+const pi = 3.456;
+console.log(pi);
+// ! pi = 4; // const değişkenini tekrar değiştiremezsin
+
+var test = 25;
+console.log(test)
---
+++ javascript data girişleri/string_ifadeler/javascript.js
@@ -0,0 +1,22 @@
+// konsola yazdırma
+console.log("hello world"); // burada string ifade yazılıyor
+
+// değişken ile yazdırma
+let email = "uzeyiryariz13@gmail.com"; // email değişkene alınıyor
+console.log(email); // email konsola yazdılıyor
+
+// birleştirme ile yazdırma
+let ad = "uzeyir"; // birleştirme ad için değişken alınıyor
+let soyad = "yariz"; // birleştirme soyad için değişken alınıyor
+let adsoyad = ad + "-" + soyad; // birleştirme için bir değişken atanıyor
+console.log(adsoyad); // atanan değişken konsola yazdılıyor 
+
+// karakter seçme
+let isim = "Azura";
+console.log(isim[0]);
+
+let i = 0;
+while(i => isim.length){
+	console.log(i);
+	i++;
+}
