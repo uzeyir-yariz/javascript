ab3afc8159bd46e35488342523733c0bf483f593
uzeyir-yariz <uzeyiryariz13@gmail.com>
2024-07-17 21:33:36 +0300

    for, some update

---
+++ 01-kosullar-donguler/for/javascript.js
@@ -0,0 +1,17 @@
+const ayrac = "-----------------";
+
+for (let i = 0; i <= 5; i++) {
+    console.log(i);
+}
+
+console.log(ayrac);
+
+const arr = [
+    "yusuf",
+    "efe",
+    "üzeyir"
+];
+
+for (let i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+}
