aea4cd5c786c7d2db04074ded3948185a6332362
uzeyir-yariz <uzeyiryariz13@gmail.com>
2024-07-17 21:04:06 +0300

    karsılastırma

---
+++ javascript data girişleri/karsılastırma/javascript.js
@@ -0,0 +1,18 @@
+const ayrac = "------------------------"
+let yas = 18;
+
+console.log(
+    yas ==   18,
+    yas ==  "18",
+    yas === "18"
+);
+
+console.log(ayrac);
+
+let str_number = "22"; 
+console.log(str_number, str_number + 5, typeof str_number);
+
+console.log(ayrac);
+
+let number = 12;
+console.log(String(number));
