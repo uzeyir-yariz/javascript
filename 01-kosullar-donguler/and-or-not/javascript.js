1ebcabf3b3342be24f3110fd1c8e09c1cdb79c7d
uzeyir-yariz <uzeyiryariz13@gmail.com>
2024-07-17 22:15:33 +0300

    and-or-not

---
+++ 01-kosullar-donguler/and-or-not/javascript.js
@@ -0,0 +1,11 @@
+const user_data = {
+    user: "user",
+    pass: "1234"
+};
+
+let user = "user";
+let pass = "1234";
+
+if (user_data.user == user && user_data.pass == pass){
+    console.log("✔");
+} else { console.log("❌") }
