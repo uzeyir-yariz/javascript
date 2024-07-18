9ebf1e456eae0874150d99cdc639f9778a897243
uzeyir-yariz <uzeyiryariz13@gmail.com>
2024-07-18 09:22:34 +0300

    switch case

---
+++ 01-kosullar-donguler/switch-case/javascript.js
@@ -0,0 +1,12 @@
+const text = "AA";
+
+switch (text) {
+  case "AA":
+    console.log("AA");
+    break;
+  case "BB":
+    console.log("BB");
+    break;
+  default:
+    console.log("default");
+}
