cb25e30c7e89638c6194d075b3e5b46f62a6c743
uzeyir-yariz <uzeyiryariz13@gmail.com>
2024-07-20 00:01:17 +0300

    getelement

---
+++ 04-javascript-DOM/getelement/javascript.js
@@ -0,0 +1,8 @@
+const ul = document.getElementById('ul');
+console.log(ul);
+
+const err = document.getElementsByClassName('err');
+console.log(err);
+
+const tags = document.getElementsByTagName('span');
+console.log(tags)
