b030ccd2ef4e91e6319dd62ecee87b0287212105
uzeyir-yariz <uzeyiryariz13@gmail.com>
2024-07-19 22:25:23 +0300

    Math-object

---
+++ 03-javascript-object/math-object/javascript.js
@@ -0,0 +1,11 @@
+console.log(Math);
+console.log(Math.PI);
+
+let num = 5.5;
+console.log(Math.round(num));
+console.log(Math.floor(num));
+console.log(Math.ceil(num));
+console.log(Math.trunc(num)); // decilmal kısmını yok et
+
+const rand = Math.random();
+console.log(Math.round(rand*100));
