56e7c08e5203266de3bb12fe542b0e791f3162c6
uzeyir-yariz <uzeyiryariz13@gmail.com>
2024-07-25 18:12:06 +0300

    push-and-pop-method

---
+++ 08-javascript-most-used-method/push-and-pop-method/javascript.js
@@ -0,0 +1,11 @@
+// * uzeyir-yariz {250720241808} * \\
+// > push-and-pop-method
+
+const points = [70, 50, 45, 25, 45, 20, 78, 65, 41, 15];
+points.push(90); // en sona yeni eleman ekler
+console.log(points); 
+
+points.pop(); // en sonda ki elemanın diziden çıkarır
+console.log(points);
+
+// * uzeyir-yariz {250720241811} * \\
