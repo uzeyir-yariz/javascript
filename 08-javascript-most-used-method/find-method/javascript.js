b5f4a2acc1d17678cd3e9e014f934aeb0d663aa9
uzeyir-yariz <uzeyiryariz13@gmail.com>
2024-07-25 18:06:45 +0300

    find-method

---
+++ 08-javascript-most-used-method/find-method/javascript.js
@@ -0,0 +1,11 @@
+// * uzeyir-yariz {250720241803} * \\
+
+const points = [70, 50, 45, 25, 45, 20, 78, 65, 41, 15];
+
+const high_point = points.find((point) => {
+    return point > 70;
+});
+
+console.log(high_point)
+
+// * uzeyir-yariz {250720241806} * \\
