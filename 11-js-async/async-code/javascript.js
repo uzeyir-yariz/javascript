e86b3ff4bc33bb97c527bfb0621a918a3d64964d
uzeyir-yariz <uzeyiryariz13@gmail.com>
2024-07-29 09:29:23 +0300

    async-code

---
+++ 11-js-async/async-code/javascript.js
@@ -0,0 +1,16 @@
+// * uzeyir-yariz {290720240852} * \\
+// > {async-code}
+
+console.log(1);
+console.log(2);
+
+setTimeout(() => {
+    console.log("Azura Krya")
+}, 1000);
+
+console.log(3);
+console.log(4);
+
+//  önce sayılar sonra "Azura Krya" isimli log yazılıyor
+
+// * uzeyir-yariz {290720240929} * \\
