dfb6b07626ae8582003e18fd8f6482cad35ecac0
uzeyir-yariz <uzeyiryariz13@gmail.com>
2024-07-31 09:33:05 +0300

    class

---
+++ 13-OOP/class/javascript.js
@@ -0,0 +1,24 @@
+// * uzeyir-yariz {310720240921} * \\
+// > {class}
+
+class User {
+  constructor(username, email) {
+    this.username = username;
+    this.email = email;
+  }
+
+  login() {
+    return `${this.username} giriş yaptı`;
+  }
+
+  logout() {
+    return `${this.username} çıkış yaptı`;
+  }
+}
+
+const user_1 = new User("Üzeyir", "uzeyiryariz13@gmail.com");
+const user_2 = new User("Arthur", "art@gmail.com");
+// console.log(user_1, user_2);
+console.log(user_1.login(), user_2.logout());
+
+// * uzeyir-yariz {date} * \\
