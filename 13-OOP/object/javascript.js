e87797b95c81d980fde20864017c03e135fd3f6e
uzeyir-yariz <uzeyiryariz13@gmail.com>
2024-07-31 09:20:44 +0300

    object

---
+++ 13-OOP/object/javascript.js
@@ -0,0 +1,32 @@
+// * uzeyir-yariz {310720240914} * \\
+// > {object}
+
+const user_1 = {
+  username: "Arthur",
+  email: "Art@gmail.com",
+  login() {
+    console.log("Arthur giriş yaptı");
+  },
+  logout() {
+    console.log("Arthur çıkış yaptı");
+  },
+};
+
+console.log(user_1.username, user_1.email);
+user_1.login();
+
+const user_2 = {
+  username: "Paul",
+  email: "pp@gmail.com",
+  login() {
+    console.log("Paul giriş yaptı");
+  },
+  logout() {
+    console.log("Paul çıkış yaptı");
+  },
+};
+
+console.log(user_2.username, user_2.email);
+user_2.login();
+
+// * uzeyir-yariz {310720240920} * \\
