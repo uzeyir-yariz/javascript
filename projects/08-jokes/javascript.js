3b557af6e54fbe4a99c0d87de966cf8f85d14402
uzeyir-yariz <uzeyiryariz13@gmail.com>
2024-08-03 15:09:44 +0300

    jokes

---
+++ projects/08-jokes/javascript.js
@@ -0,0 +1,30 @@
+const joke_text = document.getElementById("joke_text")
+const joke_btn = document.getElementById("joke_btn")
+
+const key = "https://api-ninjas.com"; // hesap açıp key buraya yapıştırmak gerekiyor
+const apiURL = "https://api.api-ninjas.com/v1/jokes"
+const opt = {
+    method: "GET",
+    headers: {
+        "X-Api-Key": key
+    }
+}
+
+// bunu async yapmam gerekiyor sebebi API çekilirken bekleme süresi var onu bekleyene kadar işlem yapılmaması gerekiyor tek tek kodları yazacağım aşağıda ki gibi
+
+async function make_joke(){
+
+    joke_text.textContent = "ugh..."
+    joke_btn.disabled = true;
+    joke_btn.textContent = "wait a second...";
+
+
+    const response = await fetch(apiURL, opt);
+    const data = await response.json();
+    joke_text.textContent = data[0].joke;
+
+    joke_btn.disabled = false;
+    joke_btn.textContent = "tell me a joke";
+}
+
+joke_btn.addEventListener("click", make_joke)
